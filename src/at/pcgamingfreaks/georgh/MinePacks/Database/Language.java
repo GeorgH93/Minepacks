1752fcc5bef19d87469c6fe82a01c4dd8967f928
Georg <georg@hagen.at>
2015-01-08 23:30:59 +0100

    Copyright update to 2015

--- src/at/pcgamingfreaks/georgh/MinePacks/Database/Language.java
+++ src/at/pcgamingfreaks/georgh/MinePacks/Database/Language.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2014 GeorgH93
+ *   Copyright (C) 2014-2015 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
