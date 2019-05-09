439b38a4e1392b75de66b299962d113efdf3a83e
GeorgH93 <git@georgh93.eu>
2019-05-10 01:40:49 +0200

    Init API release now in it's own branch!

---
+++ README.md
@@ -0,0 +1,49 @@
+<!-- Variables (this block will not be visible in the readme -->
+[banner]: https://pcgamingfreaks.at/images/minepacks.png
+[spigot]: https://www.spigotmc.org/resources/minepacks.19286/
+[license]: https://github.com/GeorgH93/Minepacks/blob/master/LICENSE
+[licenseImg]: https://img.shields.io/github/license/GeorgH93/Minepacks.svg
+[ci]: https://ci.pcgamingfreaks.at/job/Minepacks%20API/
+[ciImg]: https://ci.pcgamingfreaks.at/job/Minepacks%20API/badge/icon
+[apiVersionImg]: https://img.shields.io/badge/dynamic/xml.svg?label=api-version&query=%2F%2Flatest[1]&url=https%3A%2F%2Frepo.pcgamingfreaks.at%2Frepository%2Fmaven-releases%2Fat%2Fpcgamingfreaks%2FMinepacks-API%2Fmaven-metadata.xml
+[apiJavaDoc]: https://ci.pcgamingfreaks.at/job/Minepacks%20API/javadoc/
+[apiBuilds]: https://ci.pcgamingfreaks.at/job/Minepacks%20API/
+<!-- End of variables block -->
+
+[![Logo][banner]][spigot]
+
+This branch holds the API for the Minepacks plugin.
+
+[![ciImg]][ci] [![apiVersionImg]][apiJavaDoc] [![licenseImg]][license]
+
+## Adding it to your plugin
+### Maven
+The API is available through maven.
+#### Repository:
+```
+<repository>
+	<id>pcgf-repo</id>
+	<url>https://repo.pcgamingfreaks.at/repository/everything</url>
+</repository>
+```
+#### Dependency:
+```
+<!-- Minepacks API -->
+<dependency>
+    <groupId>at.pcgamingfreaks</groupId>
+    <artifactId>Minepacks-API</artifactId>
+    <version>2.0</version><!-- Check api-version shield for newest version -->
+</dependency>
+```
+
+### Build from source:
+```
+git clone https://github.com/GeorgH93/Minepacks.git
+cd Minepacks
+git checkout --track origin/API
+mvn clean install
+```
+
+## Links
+* [JavaDoc][apiJavaDoc]
+* [API Build Server][apiBuilds]
---
+++ src/at/pcgamingfreaks/Minepacks/Bukkit/API/MinepacksPlugin.java
@@ -0,0 +1,141 @@
+/*
+ *   Copyright (C) 2019 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks.Minepacks.Bukkit.API;
+
+import at.pcgamingfreaks.Version;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@SuppressWarnings("unused")
+public interface MinepacksPlugin
+{
+	/**
+	 * Gets the instance of the minepacks plugin.
+	 * WARNING use this function at your own risk! If the plugin is not installed the MinepacksPlugin class will be unknown!
+	 *
+	 * @return The instance of the minepacks plugin.
+	 */
+	static @Nullable MinepacksPlugin getInstance()
+	{
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("Minepacks");
+		return (plugin instanceof MinepacksPlugin && plugin.isEnabled()) ? (MinepacksPlugin) plugin : null;
+	}
+
+	/**
+	 * Gets the currently running {@link Version} of the plugin.
+	 * Version 0.0 if plugin is not loaded or enabled.
+	 *
+	 * @return The currently running version of the plugin.
+	 */
+	static @NotNull Version getVersion()
+	{
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("Minepacks");
+		if(plugin instanceof MinepacksPlugin && plugin.isEnabled())
+		{
+			return new Version(plugin.getDescription().getVersion());
+		}
+		return new Version("0.0");
+	}
+
+	/**
+	 * Let a given player open the backpack of an other player.
+	 *
+	 * @param opener   The player who opens the backpack.
+	 * @param owner    The owner of the backpack that should be opened.
+	 * @param editable Defines if the player who has opened the backpack can change the items inside.
+	 */
+	void openBackpack(@NotNull final Player opener, @NotNull final OfflinePlayer owner, final boolean editable);
+
+	/**
+	 * Let a given player open a given {@link Backpack}.
+	 *
+	 * @param opener   The player who opens the backpack.
+	 * @param backpack The backpack to be opened. null will result in an error message for the player.
+	 * @param editable Defines if the player who has opened the backpack can change the items inside.
+	 */
+	void openBackpack(@NotNull final Player opener, @Nullable final Backpack backpack, boolean editable);
+
+	/**
+	 * Let a given player open the backpack of an other player.
+	 *
+	 * @param opener   The player who opens the backpack.
+	 * @param owner    The owner of the backpack that should be opened.
+	 * @param editable Defines if the player who has opened the backpack can change the items inside.
+	 * @param title    Custom title for the backpack (will be shown to the player who opened the backpack.
+	 */
+	void openBackpack(@NotNull final Player opener, @NotNull final OfflinePlayer owner, final boolean editable, @Nullable String title);
+
+	/**
+	 * Let a given player open a given {@link Backpack}.
+	 *
+	 * @param opener   The player who opens the backpack.
+	 * @param backpack The backpack to be opened. null will result in an error message for the player.
+	 * @param editable Defines if the player who has opened the backpack can change the items inside.
+	 * @param title    Custom title for the backpack (will be shown to the player who opened the backpack.
+	 */
+	void openBackpack(@NotNull final Player opener, @Nullable final Backpack backpack, boolean editable, @Nullable String title);
+
+	/**
+	 * Retrieves the backpack for a given player.
+	 * This method only returns a backpack if it is in the cache.
+	 *
+	 * @param owner The player who's backpack should be retrieved.
+	 * @return The backpack of the given player. null if the backpack is in the cache.
+	 */
+	@Nullable Backpack getBackpackCachedOnly(@NotNull final OfflinePlayer owner);
+
+	/**
+	 * Retrieves the backpack for a given player.
+	 * This method runs async! The result will be delivered with a callback.
+	 * If no backpack exists a new one will be created.
+	 *
+	 * @param owner The player who's backpack should be retrieved.
+	 * @param callback The callback delivering the result of the request.
+	 */
+	void getBackpack(@NotNull final OfflinePlayer owner, @NotNull final Callback<Backpack> callback);
+
+	/**
+	 * Retrieves the backpack for a given player.
+	 * This method runs async! The result will be delivered with a callback.
+	 *
+	 * @param owner The player who's backpack should be retrieved.
+	 * @param callback The callback delivering the result of the request.
+	 * @param createNewIfNotExists If set to true, a new backpack will be created if there currently is no backpack for this player.
+	 */
+	void getBackpack(@NotNull final OfflinePlayer owner, @NotNull final Callback<Backpack> callback, boolean createNewIfNotExists);
+
+	/**
+	 * Gets the command manager of the Minepacks plugin.
+	 *
+	 * @return The command manager instance.
+	 */
+	MinepacksCommandManager getCommandManager();
+
+	/**
+	 * Checks if the player is allowed to open a backpack based on is permissions and current game-mode.
+	 *
+	 * @param player The player to be checked.
+	 * @return True if the player can use a backpack. False if not.
+	 */
+	boolean isPlayerGameModeAllowed(Player player);
+}
