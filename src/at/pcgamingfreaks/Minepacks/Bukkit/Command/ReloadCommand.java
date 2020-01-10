5a165f3dc6201a54ab374409e6365ed1c9297be7
GeorgH93 <git@georgh93.eu>
2020-01-10 02:57:04 +0100

    Do not broadcast reload messages (Fixes #60)

--- src/at/pcgamingfreaks/Minepacks/Bukkit/Command/ReloadCommand.java
+++ src/at/pcgamingfreaks/Minepacks/Bukkit/Command/ReloadCommand.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2019 GeorgH93
+ *   Copyright (C) 2020 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -42,9 +42,9 @@ public class ReloadCommand extends MinepacksCommand
 	@Override
 	public void execute(@NotNull CommandSender sender, @NotNull String mainCommandAlias, @NotNull String alias, @NotNull String[] args)
 	{
-		messageReloading.broadcast();
+		messageReloading.send(sender);
 		((Minepacks) plugin).reload();
-		messageReloaded.broadcast();
+		messageReloaded.send(sender);
 	}
 
 	@Override
