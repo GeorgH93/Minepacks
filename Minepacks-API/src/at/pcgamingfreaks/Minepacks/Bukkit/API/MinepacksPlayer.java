6cbfcc69fb8dfb7bb05ee7ca8437ed0db6687ead
GeorgH93 <git@georgh93.eu>
2020-09-27 12:28:18 +0200

    Add javadoc

--- Minepacks-API/src/at/pcgamingfreaks/Minepacks/Bukkit/API/MinepacksPlayer.java
+++ Minepacks-API/src/at/pcgamingfreaks/Minepacks/Bukkit/API/MinepacksPlayer.java
@@ -25,25 +25,80 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
+/**
+ * Represents a player in the Minepacks plugin.
+ */
 public interface MinepacksPlayer
 {
+	/**
+	 * Gets the name of the player.
+	 *
+	 * @return The name of the player.
+	 */
 	@NotNull String getName();
 
+	/**
+	 * Gets the {@link UUID} of the player.
+	 *
+	 * @return The {@link UUID} of the player.
+	 */
 	@NotNull UUID getUUID();
 
+	/**
+	 * Gets the {@link OfflinePlayer} object of the player.
+	 *
+	 * @return The {@link OfflinePlayer} object of the player.
+	 */
 	@NotNull OfflinePlayer getPlayer();
 
+	/**
+	 * Gets the {@link Player} object of the player.
+	 *
+	 * @return The {@link Player} object of the player. Null if the player is offline.
+	 */
 	@Nullable Player getPlayerOnline();
 
+	/**
+	 * Gets the display name of the player.
+	 * If the player is offline Gray + Name will be used instead.
+	 *
+	 * @return The display name of the player.
+	 */
 	@NotNull String getDisplayName();
 
+	/**
+	 * Checks if the player has a given permission.
+	 *
+	 * @param permission The permission that should be checked.
+	 * @return True if the player does not have the permission. False if the player does not have the permission or is offline.
+	 */
 	boolean hasPermission(final @NotNull String permission);
 
+	/**
+	 * Checks if the player is online.
+	 *
+	 * @return True if the player is online. False if not.
+	 */
 	boolean isOnline();
 
+	/**
+	 * Sets the style of the players backpack shortcut.
+	 *
+	 * @param style The style of the backpack. "default" to use the configured default style. "none" to disable the backpack shortcut for the player.
+	 */
 	void setBackpackStyle(final @NotNull String style);
 
+	/**
+	 * Gets the backpack shortcut item for the player.
+	 *
+	 * @return The backpack shortcut item for the player. null if the shortcut item is disabled for the player.
+	 */
 	@Nullable ItemStack getBackpackItem();
 
+	/**
+	 * Gets the {@link Backpack} of the player.
+	 *
+	 * @return The players {@link Backpack}.
+	 */
 	@NotNull Backpack getBackpack();
 }
