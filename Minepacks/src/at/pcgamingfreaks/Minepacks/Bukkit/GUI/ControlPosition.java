e05cd13126b9371bbca0840606501e50b1a78754
GeorgH93 <git@georgh93.eu>
2021-07-20 21:17:54 +0200

    Extend ControlPosition enum

--- Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/GUI/ControlPosition.java
+++ Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/GUI/ControlPosition.java
@@ -17,10 +17,52 @@
 
 package at.pcgamingfreaks.Minepacks.Bukkit.GUI;
 
+import lombok.Getter;
+
 public enum ControlPosition
 {
-	TOP_LEFT,
-	TOP_RIGHT,
-	BOTTOM_LEFT,
-	BOTTOM_RIGHT
+	// Horizontal controls
+	TOP_LEFT(true),
+	TOP_RIGHT(true),
+	BOTTOM_LEFT(true),
+	BOTTOM_RIGHT(true),
+
+	// Vertical controls
+	LEFT_TOP(false),
+	LEFT_BOTTOM(false),
+	RIGHT_TOP(false),
+	RIGHT_BOTTOM(false);
+
+	@Getter private final boolean vertical, horizontal;
+
+	ControlPosition(boolean horizontal)
+	{
+		this.horizontal = horizontal;
+		this.vertical = !horizontal;
+	}
+
+	public int[] getControlIds(int controlCount, int rowCount)
+	{
+		assert(controlCount <= 9);
+		int startId = -1, stride = isVertical() ? 9 : 1;
+		switch(this)
+		{
+			case TOP_LEFT: startId = 0; break;
+			case TOP_RIGHT: startId = 9 - controlCount; break;
+			case BOTTOM_LEFT: startId = (rowCount - 1) * 9; break;
+			case BOTTOM_RIGHT: startId = (rowCount * 9) - controlCount; break;
+			case LEFT_TOP: startId = 0; break;
+			case LEFT_BOTTOM: startId = (rowCount - controlCount) * 9; break;
+			case RIGHT_TOP: startId = 8; break;
+			case RIGHT_BOTTOM: startId = ((rowCount - controlCount) * 9) + 8; break;
+		}
+		assert(startId != -1);
+
+		int[] ids = new int[controlCount];
+		for(int i = 0, id = startId; i < controlCount; i++, id += stride)
+		{
+			ids[i] = id;
+		}
+		return ids;
+	}
 }
---
+++ Minepacks/test/src/at/pcgamingfreaks/Minepacks/Bukkit/GUI/ControlPositionTest.java
@@ -0,0 +1,64 @@
+/*
+ *   Copyright (C) 2022 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks.Minepacks.Bukkit.GUI;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControlPositionTest
+{
+	@Test
+	void testGetControlIds()
+	{
+		assertArrayEquals(new int[]{0, 1, 2, 3, 4}, ControlPosition.TOP_LEFT.getControlIds(5, 6));
+		assertArrayEquals(new int[]{4, 5, 6, 7, 8}, ControlPosition.TOP_RIGHT.getControlIds(5, 6));
+		assertArrayEquals(new int[]{45, 46, 47}, ControlPosition.BOTTOM_LEFT.getControlIds(3, 6));
+		assertArrayEquals(new int[]{51, 52, 53}, ControlPosition.BOTTOM_RIGHT.getControlIds(3, 6));
+		assertArrayEquals(new int[]{0, 9, 18}, ControlPosition.LEFT_TOP.getControlIds(3, 6));
+		assertArrayEquals(new int[]{27, 36, 45}, ControlPosition.LEFT_BOTTOM.getControlIds(3, 6));
+		assertArrayEquals(new int[]{8, 17, 26}, ControlPosition.RIGHT_TOP.getControlIds(3, 6));
+		assertArrayEquals(new int[]{35, 44, 53}, ControlPosition.RIGHT_BOTTOM.getControlIds(3, 6));
+	}
+
+	@Test
+	void testIsVertical()
+	{
+		assertFalse(ControlPosition.TOP_LEFT.isVertical());
+		assertFalse(ControlPosition.TOP_RIGHT.isVertical());
+		assertFalse(ControlPosition.BOTTOM_LEFT.isVertical());
+		assertFalse(ControlPosition.BOTTOM_RIGHT.isVertical());
+		assertTrue(ControlPosition.LEFT_TOP.isVertical());
+		assertTrue(ControlPosition.LEFT_BOTTOM.isVertical());
+		assertTrue(ControlPosition.RIGHT_TOP.isVertical());
+		assertTrue(ControlPosition.RIGHT_BOTTOM.isVertical());
+	}
+
+	@Test
+	void testIsHorizontal()
+	{
+		assertTrue(ControlPosition.TOP_LEFT.isHorizontal());
+		assertTrue(ControlPosition.TOP_RIGHT.isHorizontal());
+		assertTrue(ControlPosition.BOTTOM_LEFT.isHorizontal());
+		assertTrue(ControlPosition.BOTTOM_RIGHT.isHorizontal());
+		assertFalse(ControlPosition.LEFT_TOP.isHorizontal());
+		assertFalse(ControlPosition.LEFT_BOTTOM.isHorizontal());
+		assertFalse(ControlPosition.RIGHT_TOP.isHorizontal());
+		assertFalse(ControlPosition.RIGHT_BOTTOM.isHorizontal());
+	}
+}
