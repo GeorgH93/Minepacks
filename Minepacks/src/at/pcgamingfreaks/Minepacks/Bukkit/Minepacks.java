4e8651cfaafa9ea85d4d7ce536a1268f459ef2c5
GeorgH93 <git@georgh93.eu>
2023-03-14 23:29:34 +0100

    Add support for MC 1.19.4

--- Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Minepacks.java
+++ Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Minepacks.java
@@ -118,7 +118,7 @@ public class Minepacks extends JavaPlugin implements MinepacksPlugin, IPlugin
 
 	private boolean checkMcVersion()
 	{
-		if(MCVersion.is(MCVersion.UNKNOWN) || !MCVersion.isUUIDsSupportAvailable() || MCVersion.isNewerThan(MCVersion.MC_NMS_1_19_R2))
+		if(MCVersion.is(MCVersion.UNKNOWN) || !MCVersion.isUUIDsSupportAvailable() || MCVersion.isNewerThan(MCVersion.MC_NMS_1_19_R3))
 		{
 			this.warnOnVersionIncompatibility();
 			this.setEnabled(false);
