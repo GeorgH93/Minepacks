GeorgH93/Minepacks

commit 8fe2365929cdfb29f95f1122920d2c7f8589493c

Backup backpacks that failed to load

Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Database/SQL.java
@@ -249,7 +249,8 @@ public abstract class SQL extends Database
 		plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
 			try(Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(queryGetBP))
 			{
-				ps.setString(1, getPlayerFormattedUUID(player));
+				final String playerUUID = getPlayerFormattedUUID(player);
+				ps.setString(1, fieldPlayerUUID);
 				final int bpID, version;
 				final byte[] data;
 				try(ResultSet rs = ps.executeQuery())
@@ -269,6 +270,10 @@ public abstract class SQL extends Database
 				}
 
 				ItemStack[] its = itsSerializer.deserialize(data, version);
+				if (data != null && data.length != 0 && its == null)
+				{
+					writeBackup(player.getName(), playerUUID, version, data);
+				}
 				final Backpack backpack = (its != null) ? new Backpack(player, its, bpID) : null;
 				plugin.getServer().getScheduler().runTask(plugin, () -> {
 					if(backpack != null)

Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Database/Database.java
@@ -133,7 +133,7 @@ public abstract class Database implements Listener
 		writeBackup(backpack.getOwner().getName(), getPlayerFormattedUUID(backpack.getOwner()), itsSerializer.getUsedSerializer(), itsSerializer.serialize(backpack.getInventory()));
 	}
 
-	protected void writeBackup(@Nullable String userName, @NotNull String userIdentifier, final int usedSerializer, final @NotNull byte[] data)
+	protected void writeBackup(@Nullable String userName, @NotNull String userIdentifier, final int usedSerializer, final byte[] data)
 	{
 		if(userIdentifier.equalsIgnoreCase(userName)) userName = null;
 		if(userName != null) userIdentifier = userName + "_" + userIdentifier;

commit 01706e4ae4951a106f89961635b6e9d89d449870

Fix loading of backpacks (regression introduced with 8fe2365929cdfb29f95f1122920d2c7f8589493c)

Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Database/SQL.java
@@ -250,7 +250,7 @@ public abstract class SQL extends Database
 			try(Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(queryGetBP))
 			{
 				final String playerUUID = getPlayerFormattedUUID(player);
-				ps.setString(1, fieldPlayerUUID);
+				ps.setString(1, playerUUID);
 				final int bpID, version;
 				final byte[] data;
 				try(ResultSet rs = ps.executeQuery())