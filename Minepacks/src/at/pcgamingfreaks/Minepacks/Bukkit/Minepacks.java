dcd1db14add29fe60d1cbb2a8a174d63addaf75b
GeorgH93 <git@georgh93.eu>
2025-04-11 18:00:18 +0200

    Add support for MC 1.21.5

--- Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Minepacks.java
+++ Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Minepacks.java
@@ -129,7 +129,7 @@ public class Minepacks extends JavaPlugin implements MinepacksPlugin, IPlugin
 			getLogger().warning("Paper support is experimental! Use at your own risk!");
 			getLogger().warning("No guarantee for data integrity! Backup constantly!");
 		}
-		if (MCVersion.is(MCVersion.UNKNOWN) || !MCVersion.isUUIDsSupportAvailable() || MCVersion.isNewerThan(MCVersion.MC_NMS_1_21_R3))
+		if (MCVersion.is(MCVersion.UNKNOWN) || !MCVersion.isUUIDsSupportAvailable() || MCVersion.isNewerThan(MCVersion.MC_NMS_1_21_R4))
 		{
 			this.warnOnVersionIncompatibility();
 			this.setEnabled(false);
