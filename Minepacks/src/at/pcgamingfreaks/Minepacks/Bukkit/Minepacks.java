760adc85c7b7d1cd5d4e6c18875c98ad6497939e
GeorgH93 <git@georgh93.eu>
2025-12-17 11:11:11 +0100

    Add support for MC 1.21.11

--- Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Minepacks.java
+++ Minepacks/src/at/pcgamingfreaks/Minepacks/Bukkit/Minepacks.java
@@ -134,7 +134,7 @@ public class Minepacks extends JavaPlugin implements MinepacksPlugin, IPlugin
 			getLogger().warning("No guarantee for data integrity! Backup constantly!");
 		}
 		// DO NOT REMOVE THIS! This is protecting your data! To add support for a new version, update PCGF PluginLib and then update the last version check!
-		if (MCVersion.is(MCVersion.UNKNOWN) || !MCVersion.isUUIDsSupportAvailable() || MCVersion.isNewerThan(MCVersion.MC_NMS_1_21_R6))
+		if (MCVersion.is(MCVersion.UNKNOWN) || !MCVersion.isUUIDsSupportAvailable() || MCVersion.isNewerThan(MCVersion.MC_NMS_1_21_R7))
 		{
 			this.warnOnVersionIncompatibility();
 			this.setEnabled(false);
